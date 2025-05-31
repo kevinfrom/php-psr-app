388ea8cfeac56bc807c124285472c449d2392735
Kevin From <kevin@kevinfrom.dk>
2025-05-31 21:33:53 +0200

    created logs and tmp directories

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -6,7 +6,7 @@ const DS = DIRECTORY_SEPARATOR;
 define('ROOT_DIR', dirname(__DIR__));
 
 const BOOTSTRAP_DIR = ROOT_DIR . DS . 'bootstrap';
-
+const LOGS_DIR = ROOT_DIR . DS . 'logs';
 const PUBLIC_DIR = ROOT_DIR . DS . 'public';
-
+const TMP_DIR = ROOT_DIR . DS . 'tmp';
 const VENDOR_DIR = ROOT_DIR . DS . 'vendor';
