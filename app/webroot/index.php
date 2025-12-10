6081fd38e68468858776904ae13d19dd8b2e6455
Kevin From <kf+bitbucket@vestjyskmarketing.dk>
2025-12-10 14:27:18 +0100

    created front controller

---
+++ app/webroot/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Front controller.
+ * 
+ * This handles all requests and dispatches the application.
+ */
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+
+echo 'test';
